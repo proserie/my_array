7752d8e88ce03773ca229b3f974d214389f5aebf
Patrick Roserie <proserie@vmware.com>
2018-09-25 17:24:08 -0400

    update pydocs

--- my_array/__init__.py
+++ my_array/__init__.py
@@ -18,16 +18,52 @@ class Array:
 		self.data = data
 
 	def sum(self):
+		'''
+		Sums all the valuses in the array
+
+		Returns
+		-------
+		int or float
+		'''
 		return sum(self.data)
 
 	def min(self):
+		'''
+		Returns the smallest value in the array
+
+		Returns
+		-------
+		int or float
+		'''
 		return min(self.data)
 
 	def max(self):
+		'''
+		Returns the highest value in the array
+
+		Returns
+		-------
+		int or float
+
+		'''
 		return max(self.data)
 
 	def median(self):
+		'''
+		Returns the median value in the array
+
+		Returns
+		-------
+		int or float
+		'''
 		return median(self.data)
 
 	def mean(self):
+		'''
+		Returns the mean value in the array
+
+		Returns
+		-------
+		int or float
+		'''
 		return self.sum() / len(self.data)
